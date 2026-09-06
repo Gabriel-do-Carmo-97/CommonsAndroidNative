09418b5ae07963c6f1229666d603b84a29a44c5c
Gabriel do Carmo <gabriel.desenvolvedor.97@gmail.com>
2026-09-06 20:44:42 -0300

    feat(payment): configure dependencies for storage and design system

--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("commons.android.library")
+    id("commons.android.compose")
     id("commons.android.publishing")
+    id("commons.android.hilt")
 }
 
 android {
@@ -8,10 +10,27 @@ android {
 }
 
 dependencies {
+    implementation(libs.myAndroidCore)
+    implementation(libs.core.storage)
+    implementation(libs.design.system)
+    implementation(libs.ds.templates)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.turbine)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 }
